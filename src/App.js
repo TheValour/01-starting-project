b4a2f14d85a515b4365900d2057c6cf5f2ed677e
The_Valour <108991640+TheValour@users.noreply.github.com>
2023-04-28 15:52:03 +0000

    level 0 ( storaging data in localStorage )

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,15 +6,21 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  useEffect(() =>{
+    if(localStorage.getItem('LogedIn')){
+      setIsLoggedIn(true);
+    }
+  }, [])
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem('LogedIn' , '1');
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem('LogedIn');
   };
 
   return (
